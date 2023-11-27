cdb729055300e9ed11072fbf9765bd5fe14ac093
Yasin Karadavut <yasinkaradavut00@gmail.com>
2023-11-27 21:38:27 +0300

    ComplatelyFinished by Yasin and Osman

--- ComplatelyFinished/optionfunctions.py
+++ ComplatelyFinished/optionfunctions.py
@@ -2,7 +2,8 @@ import os
 import shutil
 import subprocess
 import sys
-
+from EditControlDictWindow import EditControlDictWindow
+from EditFvSchemesWindow import EditFvSchemesWindow
 from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import (
     QApplication,
@@ -58,21 +59,38 @@ class OptionsWindow(QDialog, gui.Ui_Dialog):
         subdirectories = []
 
         if selected_option == "laplacianFoam":
+            self.options_combo.setToolTip(
+                "LaplacianFoam is a solver in OpenFOAM for solving Laplace's equation and similar problems in fluid dynamics and heat transfer.")
             subdirectories = ["flange", "implicitAMI"]
         elif selected_option == "potentialFoam":
             subdirectories = ["cylinder", "pitzDaily"]
+            self.options_combo.setToolTip(
+                "potentialFoam is a solver in OpenFOAM for simulating electric potential and electrostatic fields in electrostatic problems.")
         elif selected_option == "scalarTransportFoam":
+            self.options_combo.setToolTip(
+                "scalarTransportFoam is an OpenFOAM solver for modeling the transport of scalar quantities (e.g., temperature, concentration) in fluid flows, used in various applications from heat transfer to chemical reactions.")
             subdirectories = [""]
         elif selected_option == "rhoCentralFoam":
+            self.options_combo.setToolTip(
+                "rhoCentralFoam is an OpenFOAM solver for simulating compressible, transient, and turbulent flows using a central-upwind scheme for density-based solvers.")
             subdirectories = [""]
         elif selected_option == "rhoPimpleFoam":
+            self.options_combo.setToolTip(
+                "rhoPimpleFoam is an OpenFOAM solver that's suitable for transient, incompressible flows with turbulence modeling, using the PIMPLE (PISO-SIMPLE) algorithm to solve the Navier-Stokes equations.")
             subdirectories = [""]
         elif selected_option == "rhoPorousSimpleFoam":
+            self.options_combo.setToolTip(
+                "rhoPorousSimpleFoam is an OpenFOAM solver designed for modeling flows through porous media with incompressible, turbulent fluid flow, suitable for various applications involving porous materials like filters or packed beds.")
             subdirectories = [""]
         elif selected_option == "rhoSimpleFoam":
+            self.options_combo.setToolTip(
+                "rhoSimpleFoam is an OpenFOAM solver for steady-state, incompressible flows, using the SIMPLE (Semi-Implicit Method for Pressure-Linked Equations) algorithm to solve the Navier-Stokes equations.")
             subdirectories = [""]
         elif selected_option == "icoFoam":
-            subdirectories = ["cavity"]
+            self.options_combo.setToolTip(
+                "icoFoam is an OpenFOAM solver designed for simulating incompressible, steady-state flows using a PISO (Pressure-Implicit with Splitting of Operators) algorithm, making it suitable for a wide range of fluid flow applications.")
+            subdirectories = ["cavity/cavity"]
+
 
         self.subdirectory_combo.clear()
         self.subdirectory_combo.addItems(subdirectories)
@@ -114,13 +132,30 @@ class OptionsWindow(QDialog, gui.Ui_Dialog):
                 self.accept()
                 # QMessageBox.information(self, "Success",
                 #                         f"Copying {selected_option}/{selected_subdirectory} successful.")
+                QMessageBox.information(self, "Success",
+                                        f"Copying {selected_option}/{selected_subdirectory} successful.")
 
+                # Open the EditControlDictWindow immediately after copying
+                edit_control_dict_window = EditControlDictWindow(
+                    os.path.join(destination_directory, 'system/controlDict'))
+                if edit_control_dict_window.exec() == QDialog.DialogCode.Accepted:
+                    # Handle any actions after the user edits and saves the controlDict file
+                    QMessageBox.information(self, "Success",
+                                            f"ControlDict edited successfully")
+
+                # Open the EditControlDictWindow immediately after copying
+                edit_fv_schemes_window = EditFvSchemesWindow(
+                    os.path.join(destination_directory, 'system/fvSchemes'))
+                if edit_fv_schemes_window.exec() == QDialog.DialogCode.Accepted:
+                    # Handle any actions after the user edits and saves the controlDict file
+                    QMessageBox.information(self, "Success",
+                                            f"fvSchemes edited successfully")
                 # After copying, change the current directory to the destination and run "./Allrun" in WSL
                 print(destination_directory)
                 wsl_path = '/mnt/c/' + destination_directory.replace('\\', '/').lstrip('C:').lstrip('/')
 
                 # Build the WSL command to run "./Allrun" and capture the output
-                wsl_command = "cd '{}' && source /usr/lib/openfoam/openfoam2306/etc/bashrc && ./Allrun && foamToVTK".format(
+                wsl_command = "cd '{}' && source /usr/lib/openfoam/openfoam2306/etc/bashrc && blockMesh && icoFoam && foamToVTK".format(
                     wsl_path)
 
                 # Launch a cmd window while running the WSL command and capturing the output
@@ -151,6 +186,7 @@ class OptionsWindow(QDialog, gui.Ui_Dialog):
                 output, error = process.communicate()
                 progress_text.append(output)
                 progress_text.append(error)
+                progress_dialog.exec()
 
                 QMessageBox.information(self, "Success",
                                         "Please open VTK file from your project directory in Paraview")
